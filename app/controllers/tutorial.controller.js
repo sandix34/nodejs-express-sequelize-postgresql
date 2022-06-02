d5af9808ea306410a0be38c2b0e005b99e63a6fb
sandix34 <sandrinezocca@gmail.com>
2022-06-02 11:48:56 +0200

    Create the controller to retrieve objects with condition

--- app/controllers/tutorial.controller.js
+++ app/controllers/tutorial.controller.js
@@ -33,7 +33,18 @@ exports.create = (req, res) => {
 
 // Retrieve all Tutorials from the database.
 exports.findAll = (req, res) => {
-
+    const title = req.query.title;
+    let condition = title ? { title: { [Op.iLike]: `%${title}` } } : null;
+    Tutorial.findAll({ where: condition })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving tutorials."
+            });
+        });
 };
 // Find a single Tutorial with an id
 exports.findOne = (req, res) => {
--- app/routes/tutorial.routes.js
+++ app/routes/tutorial.routes.js
@@ -1,8 +1,10 @@
+const {findAll} = require("../controllers/tutorial.controller");
 module.exports = app => {
     const tutorials = require("../controllers/tutorial.controller.js");
     let router = require("express").Router();
 
     // Create a new Tutorial
     router.post("/", tutorials.create);
+    router.get("/", tutorials.findAll);
     app.use('/api/tutorials', router);
 };
