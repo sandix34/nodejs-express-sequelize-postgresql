52c73b5c2b5bc78419e98ae1965af2275c514eb3
sandix34 <sandrinezocca@gmail.com>
2022-06-02 14:00:56 +0200

    Create the controller to find all objects by condition

--- app/routes/tutorial.routes.js
+++ app/routes/tutorial.routes.js
@@ -1,3 +1,4 @@
+const tutorials = require("../controllers/tutorial.controller");
 module.exports = app => {
     const tutorials = require("../controllers/tutorial.controller.js");
     let router = require("express").Router();
@@ -5,6 +6,7 @@ module.exports = app => {
     // Create a new Tutorial
     router.post("/", tutorials.create);
     router.get("/", tutorials.findAll);
+    router.get("/published", tutorials.findAllPublished);
     router.get("/:id", tutorials.findOne);
     router.put("/:id", tutorials.update);
     router.delete("/:id", tutorials.delete);
