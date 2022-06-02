868eb122cd9715e31c1bb3e2a07f4fad70e06691
sandix34 <sandrinezocca@gmail.com>
2022-06-02 13:40:13 +0200

    Create the controller to update a object

--- app/routes/tutorial.routes.js
+++ app/routes/tutorial.routes.js
@@ -6,5 +6,6 @@ module.exports = app => {
     router.post("/", tutorials.create);
     router.get("/", tutorials.findAll);
     router.get("/:id", tutorials.findOne);
+    router.put("/:id", tutorials.update);
     app.use('/api/tutorials', router);
 };
